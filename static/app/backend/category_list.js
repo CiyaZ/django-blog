09514e6fd4c842b873b6764860e32ed2232a4246
CiyaZ <ciyaz@qq.com>
2019-07-03 09:10:09 +0800

    :bug: 修复分类创建前台未校验

--- static/app/backend/category_list.js
+++ static/app/backend/category_list.js
@@ -1,5 +1,19 @@
 var categoryListGlobal = {};
 
+function check() {
+    var categoryNameInput = $('#category-name');
+    var categoryNameInvalid = $('#category-name-invalid');
+    categoryNameInput.removeClass('is-invalid');
+    var categoryName = categoryNameInput.val();
+    var checked = true;
+    if (categoryName.length < 1 || categoryName.length > 20) {
+        checked = false;
+        categoryNameInput.addClass('is-invalid');
+        categoryNameInvalid.text('分类名长度在1-20个字符之间');
+    }
+    return checked;
+}
+
 function handleDeleteButton(id) {
     categoryListGlobal.categoryId = id;
     $('#delete-confirm-modal').modal('show');
